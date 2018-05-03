5f91ff621fd565b3e5c1fce4f29664ea515fb59e
Your Name <you@example.com>
2018-05-04 01:13:55 +0800

    try generate midi sheet

---
+++ jpg2midi.py
@@ -0,0 +1,33 @@
+import sys
+import numpy as np
+import midi
+from PIL import Image
+import matplotlib.pyplot as plt
+
+input_path = str(sys.argv[1])
+output_path = str(sys.argv[2])
+
+piano_roll = np.array(Image.open(input_path))
+piano_roll = np.transpose(np.flip(piano_roll, 0), (1,0))
+piano_roll = np.lib.pad(piano_roll, ((0,1),(0,1)), 'constant', constant_values=((0,0),(0,0)))
+# plt.imshow(piano_roll, cmap='gray')
+piano_roll[piano_roll<100] = 0
+piano_roll[piano_roll>0] = 255
+
+pattern = midi.Pattern(resolution=4)
+track = midi.Track()
+pattern.append(track)
+lastEvent = 0
+sweep = np.zeros_like(piano_roll[0], dtype=np.bool)
+for tick, line in enumerate(piano_roll):
+    for i, note in enumerate(line):
+        if note>0 and not sweep[i]: # a note on event
+            track.append(midi.NoteOnEvent(tick=tick-lastEvent, data=[i+34 ,127]))
+            lastEvent = tick
+            sweep[i]=True
+        if note==0 and sweep[i]: # a note off event
+            track.append(midi.NoteOffEvent(tick=tick-lastEvent, data=[i+34 ,0]))
+            lastEvent = tick
+            sweep[i]=False
+track.append(midi.EndOfTrackEvent(tick=1))
+midi.write_midifile(output_path, pattern)
---
+++ midi2jpg.py
@@ -0,0 +1,75 @@
+import sys
+import os
+import numpy as np
+import scipy
+import midi
+from PIL import Image
+
+lower_bound=34
+upper_bound=97
+bounded=upper_bound-lower_bound+1 # 64
+segLen=64
+
+def Tempo2BPM(x):
+    ret = x.data[2] | x.data[1]<<8 | x.data[0]<<16
+    ret = float(60000000)/float(ret)
+    return ret
+
+prefix = str(sys.argv[1])
+output_dir = str(sys.argv[2])
+
+for filename in os.listdir(prefix):
+    fullfile = prefix+'/'+str(filename)
+    try:
+        midi_in = midi.read_midifile(fullfile)
+    except:
+        continue
+    sheet_grid = np.zeros((segLen,bounded), dtype=np.float32)
+    for track in midi_in:
+        Normal = 120.0
+        defaultRes = 4
+        ResScale = float(midi_in.resolution) / float(defaultRes)
+        speedRatio = 1.0
+        accumTick = 0.
+        firstChange = True
+        swLine = np.zeros(bounded, dtype=np.float32)
+        lastTick = 0.
+        for v in track:
+            if isinstance(v, midi.ProgramChangeEvent) and v.data[0]!=0:
+                break
+            if hasattr(v, 'tick') :
+                tick = float(v.tick)/speedRatio/ResScale
+                accumTick += tick
+                accumTick_i = int(accumTick)
+                lastTick_i = int(lastTick)
+                if accumTick_i>lastTick_i:
+                    if sheet_grid.shape[0]<accumTick_i:
+                        sheet_grid = np.lib.pad(sheet_grid, ((0,accumTick_i-lastTick_i),(0,0)), 'constant', constant_values=((0,0),(0,0)))
+                    new = sheet_grid[lastTick_i:accumTick_i] + swLine
+                    sheet_grid[lastTick_i:accumTick_i,:] = new
+                    if (isinstance(v, midi.NoteOffEvent) or isinstance(v, midi.NoteOnEvent)) and v.tick==1 and v.data[0]>=lower_bound and v.data[0]<=upper_bound:
+                        sheet_grid[max(lastTick_i,accumTick_i-1), v.data[0]-lower_bound] = 0
+                    lastTick = accumTick
+            if isinstance(v, midi.SetTempoEvent):
+                changeBPM = Tempo2BPM(v)
+                if firstChange:
+                    firstChange = False
+                    Normal = changeBPM
+                    continue
+                speedRatio = float(changeBPM)/float(Normal)
+            elif isinstance(v, midi.NoteOnEvent) and v.data[0]>=lower_bound and v.data[0]<=upper_bound and v.data[1]>0:
+                swLine[v.data[0]-lower_bound] = v.data[1]
+            elif (isinstance(v, midi.NoteOffEvent) or (isinstance(v, midi.NoteOnEvent) and v.data[1]==0)) and v.data[0]>=lower_bound and v.data[0]<=upper_bound:
+                swLine[v.data[0]-lower_bound] = 0
+    sheet_grid[sheet_grid>0] = 255
+    if np.sum(sheet_grid)==0: continue
+    while np.sum(sheet_grid[-1,:])==0:
+        sheet_grid= np.delete(sheet_grid, -1, 0) ## trim 0
+    sheet_grid = np.flip(np.transpose(np.clip(sheet_grid,0,255).astype(np.uint8), (1,0)), 0)
+    for i in xrange(0, sheet_grid.shape[1]-segLen, segLen//8):
+        note_ratio = float(np.sum(sheet_grid[:,i:i+segLen]>0)) / float(segLen*60)
+        if note_ratio < 0.0005: continue
+        im = Image.fromarray(sheet_grid[:,i:i+segLen])
+        saveto = output_dir+'/'+''.join(filename.split('.')[:-1])+'_p'+str(i)+'.jpg'
+        im.save(saveto, format='JPEG', subsampling=0, quality=100)
+        print('image saved to: ' + saveto)
