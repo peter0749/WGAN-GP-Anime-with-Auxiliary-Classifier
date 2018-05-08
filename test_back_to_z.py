bc13f62dec8d2fb9acdf0c999eef83294c8ef0ee
Your Name <you@example.com>
2018-05-09 01:15:51 +0800

    test many times

--- test_back_to_z.py
+++ test_back_to_z.py
@@ -23,10 +23,14 @@ parser.add_argument('output', metavar='output', type=str, help='')
 parser.add_argument('--model', type=str, default='./decoder.h5', required=False, help='model')
 parser.add_argument('--std', type=float, default=1.0, required=False, help='')
 parser.add_argument('--iterations', type=int, default=500, required=False, help='')
+parser.add_argument('--runs', type=int, default=10, required=False, help='')
 args = parser.parse_args()
 
 model = load_model(args.model, custom_objects={'tf':tf, 'PixelShuffler':PixelShuffler, 'up_bilinear':up_bilinear})
-img = (resize(imread(args.input), model.output_shape[-3:-1], preserve_range=True).astype(np.float32) - 127.5) / 127.5
-z, img_reconstruct = back_to_z(img, model, args.std, iterations=args.iterations, return_img=True)
-output_img = np.round(np.concatenate((np.squeeze(img), np.squeeze(img_reconstruct)), axis=1) * 127.5 + 127.5).astype(np.uint8)
-imsave(args.output, output_img)
+for i in range(args.runs):
+    print('Runs: %d / %d'%(i+1, args.runs))
+    img = (resize(imread(args.input), model.output_shape[-3:-1], preserve_range=True).astype(np.float32) - 127.5) / 127.5
+    z, img_reconstruct = back_to_z(img, model, args.std, iterations=args.iterations, return_img=True)
+    output_img = np.round(np.concatenate((np.squeeze(img), np.squeeze(img_reconstruct)), axis=1) * 127.5 + 127.5).astype(np.uint8)
+    filename, ext = os.path.splitext(args.output)
+    imsave(filename+'_%d'+ext, output_img)
